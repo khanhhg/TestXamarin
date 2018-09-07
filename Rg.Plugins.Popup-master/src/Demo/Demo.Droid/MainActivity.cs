1b90a7a4b2fa5c3ac54837ee454dedbf0ca753cb
KhanhNguyen <nhkhanhkc@gmail.com>
2018-09-07 11:56:47 +0700

    Demo

---
+++ Rg.Plugins.Popup-master/src/Demo/Demo.Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Debug = System.Diagnostics.Debug;
+
+namespace Demo.Droid
+{
+    [Activity(Label = "Demo", Icon = "@drawable/icon", /*Theme = "@style/Theme.AppCompat.Light.NoActionBar",*/ MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Rg.Plugins.Popup.Popup.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+            AppCenter.Start("{Your App Secret}", typeof(Analytics), typeof(Crashes));
+        }
+
+        public override void OnBackPressed()
+        {
+            if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
+            {
+                Debug.WriteLine("Android back button: There are some pages in the PopupStack");
+            }
+            else
+            {
+                Debug.WriteLine("Android back button: There are not any pages in the PopupStack");
+            }
+        }
+    }
+}
+
---
+++ Rg.Plugins.Popup-master/src/Demo/Demo/App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Demo.Pages;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+namespace Demo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#7dbbe6"),
+                BarTextColor = Color.White
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts AppCenter.Start("ios={Your App Secret};android={Your App Secret};uwp={Your App Secret}", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
